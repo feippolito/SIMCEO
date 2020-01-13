18860869d92490e95df497fc2aabd71dfe2bbaea
Felipe Ippolito <felipeippolito@gmail.com>
2020-01-13 14:38:08 -0800

    Working with reorder - different sample rates

---
+++ dos/rM1Pos0Controller/rM1Pos0_types.h
@@ -0,0 +1,28 @@
+/*
+ * File: rM1Pos0_types.h
+ *
+ * Code generated for Simulink model 'rM1Pos0'.
+ *
+ * Model version                  : 1.4
+ * Simulink Coder version         : 8.14 (R2018a) 06-Feb-2018
+ * C/C++ source code generated on : Mon Jan 13 14:33:08 2020
+ *
+ * Target selection: ert.tlc
+ * Embedded hardware selection: Intel->x86-64 (Windows64)
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#ifndef RTW_HEADER_rM1Pos0_types_h_
+#define RTW_HEADER_rM1Pos0_types_h_
+
+/* Forward declaration for rtModel */
+typedef struct tag_RTM_rM1Pos0_T RT_MODEL_rM1Pos0_T;
+
+#endif                                 /* RTW_HEADER_rM1Pos0_types_h_ */
+
+/*
+ * File trailer for generated code.
+ *
+ * [EOF]
+ */
