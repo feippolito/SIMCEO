e7d9670cde6c07fdcb8c08112ac1efc343c08cb5
Felipe Ippolito <felipeippolito@gmail.com>
2020-04-30 22:05:38 -0300

    fdirectory restructured - simulation_config, simulation_archive & drivers

--- dos/control.py
+++ dos/control.py
@@ -1,26 +1,5 @@
 from scipy import signal
 import pickle
-from .mpc_driver import MPC
-from .sh_aco_driver import SHAcO
-from .sh_aco_debug_driver import SHAcO_debug
-from .MountController import Mount
-from .M1LocalControl0Controller import M1LocalControl0
-from .M1LocalControl_wBM0Controller import M1LocalControl_wBM0
-from .M1Pos_HpKController import M1Pos_HpK
-from .M1HPloadcells_HpKController import M1HPloadcells_HpK
-from .M1Pos_fullHpK0Controller import M1Pos_fullHpK0
-from .M1Pos_fullHpK_SDController import M1Pos_fullHpK_SD
-from .M1loadcells_fullHpKController import M1loadcells_fullHpK
-from .M1loadcells_fullHpK_SDController import M1loadcells_fullHpK_SD
-
-from .KController import K
-from .BM2FController import BM2F
-
-from .BendingModesStatic0Controller import BendingModesStatic0
-from .M1HPloadcells0Controller import M1HPloadcells0
-from .M1Pos0Controller import M1Pos0
-from .M1PosDebug0Controller import M1PosDebug0
-from .source import Source
 import numpy as np
 
 class System:
